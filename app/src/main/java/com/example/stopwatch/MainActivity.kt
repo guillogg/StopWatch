8fd484e18c7eceea1d0c37c5544191b1d3e08695
guillogg <guillexbox2009@gmail.com>
2023-03-06 20:36:59 -0300

    iconos  + xml

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.stopwatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityMainBinding
+    private var timerStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.StartButton.setOnClickListener { startStopTimer() }
+        binding.ResetButton.setOnClickListener { resetTimer() }
+
+        serviceIntent = Intent(applicationContext,TimerService::class.java)
+    }
+
+    private fun resetTimer() {
+
+    }
+
+    private fun startStopTimer() {
+
     }
 }
